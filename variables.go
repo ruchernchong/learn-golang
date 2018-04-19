3f734fefdfd1a75cf29c41e0cce0b7284362e758
Ru Chern Chong <hello@ruchern.xyz>
2018-04-20 00:59:31 +0800

    Add some typing examples
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- variables.go
+++ variables.go
@@ -7,6 +7,9 @@ func main() {
 		title, description = "Learn Golang", "Learning the basics of Golang with me"
 	)
 
+	var chapter int = 1
+
 	fmt.Println("Title: \n", title)
 	fmt.Println("Description: \n", description)
+	fmt.Println("Chapter: \n", chapter)
 }

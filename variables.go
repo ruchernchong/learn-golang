3ceb91632f03ffaf8d3e2fcb07f3b75b1cf48f8c
Ru Chern Chong <hello@ruchern.xyz>
2018-04-20 01:05:32 +0800

    Update package using shorthand methods
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- variables.go
+++ variables.go
@@ -3,23 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		title, description = "Learn Golang", "Learning the basics of Golang with me"
-	)
+	lesson()
+}
 
-	var chapter int = 1
-	var isCompleted = true
+func lesson() {
+	title := "Learn Golang"
+	description := "Learning the basics of Golang with me"
+	chapter := 1
 
 	fmt.Println("Title: \n", title)
 	fmt.Println("Description: \n", description)
 	fmt.Println("Chapter: \n", chapter)
-	fmt.Println("Lesson completed? \n", isCompleted)
-
-	shorthand()
-}
-
-func shorthand() {
-	function := "Shorthand Function"
-
-	fmt.Println("Using", function)
 }

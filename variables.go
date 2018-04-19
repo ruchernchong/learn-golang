6a084b3808ab0efdd002a972b5960cc530257f5a
Ru Chern Chong <me@ruchern.com>
2018-04-20 00:57:14 +0800

    Add playing with variables
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		title, description = "Learn Golang", "Learning the basics of Golang with me"
+	)
+
+	fmt.Println("Title: \n", title)
+	fmt.Println("Description: \n", description)
+}
